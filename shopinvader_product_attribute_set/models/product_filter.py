8882c4209792d8b3dbbdd861c7d647d96298e922
Kévin Roche <75080572+Kev-Roche@users.noreply.github.com>
2021-06-28 15:42:30 +0200

    [FIX] product_filter.py

--- shopinvader_product_attribute_set/models/product_filter.py
+++ shopinvader_product_attribute_set/models/product_filter.py
@@ -11,7 +11,6 @@ class ProductFilter(models.Model):
 
     based_on = fields.Selection(
         selection_add=[("product_attribute", "Product Attribute")],
-        ondelete={"product_attribute": "cascade"},
     )
     product_attribute_id = fields.Many2one(
         string="Product Attribute", comodel_name="attribute.attribute"
