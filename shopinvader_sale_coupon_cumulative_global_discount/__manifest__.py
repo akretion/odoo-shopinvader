4affc8e2c68ab68cad65c056a86b950dbc0ac5cf
Florian Mounier <paradoxxx.zero@gmail.com>
2021-11-15 15:34:46 +0100

    [ADD] shopinvader_sale_coupon_cumulative_global_discount

---
+++ shopinvader_sale_coupon_cumulative_global_discount/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Akretion - Florian Mounier
+{
+    "name": "Sale Coupon Cumulative Global Discount Shopinvader Integration",
+    "summary": "Improve sale_coupon_cumulative_global_discount shopinvader "
+    "integration by recomputing promotion on coupon removal.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["sale_coupon_cumulative_global_discount", "shopinvader_sale_coupon"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "auto_install": True,
+}
---
+++ shopinvader_sale_coupon_cumulative_global_discount/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module improves sale_coupon_cumulative_global_discount shopinvader integration by recomputing promotion on coupon removal.
