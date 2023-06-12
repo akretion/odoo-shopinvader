7c7d35fe681c51a0a0c9479d992ff4c80911e0e6
Sébastien BEAU <sebastien.beau@akretion.com>
2023-06-12 12:52:22 +0200

    shopinvader_partner_auth: create shopinvader partner auth and do not export it to locomotive

--- shopinvader_partner_auth/services/authenticable.py
+++ shopinvader_partner_auth/services/authenticable.py
@@ -11,3 +11,17 @@ class PortalAuthenticable(Component):
 
     def _get_directory(self):
         return self.shopinvader_backend.sudo().directory_auth_id
+
+    def _create_partner_auth(self, params):
+        partner_auth = super()._create_partner_auth(params)
+        # Create shopinvader binding as it's mandatory for V14
+        # Do not export it to locomotive as it's not needed
+        self.env["shopinvader.partner"].sudo().with_context(
+            no_connector_export=True
+        ).create(
+            {
+                "record_id": partner_auth.partner_id.id,
+                "backend_id": self.shopinvader_backend.id,
+            }
+        )
+        return partner_auth
