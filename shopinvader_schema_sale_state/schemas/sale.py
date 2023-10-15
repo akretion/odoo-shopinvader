9a00191da99c86cc4204e66294076c1ae6929e8f
Sébastien BEAU <sebastien.beau@akretion.com>
2023-10-15 18:15:08 +0200

    shopinvader_api_cart: quick V14 backport, add sudo for write as too many compute field cause error

--- shopinvader_schema_sale_state/schemas/sale.py
+++ shopinvader_schema_sale_state/schemas/sale.py
@@ -7,7 +7,30 @@ from odoo.addons.shopinvader_schema_sale.schemas import sale
 
 class Sale(sale.Sale, extends=True):
     @classmethod
+    def _get_futur_state(cls, odoo_rec):
+        # V16 shopinvader_delivery_state
+        if "delivery_state" in odoo_rec._fields:
+            if odoo_rec.delivery_state == "partially":
+                return "delivery_partial"
+            elif odoo_rec.delivery_state == "done":
+                return "delivery_done"
+        # V16 shopinvader_api_quotation
+        if odoo_rec.typology == "quotation" and odoo_rec.state == "draft":
+            return "estimating"
+        elif odoo_rec.typology == "quotation" and odoo_rec.state == "sent":
+            return "estimated"
+        # V16 shopinvader_sale_state
+        elif odoo_rec.state == "cancel":
+            return "cancel"
+        elif odoo_rec.state == "done":
+            return "delivery_full"
+        elif odoo_rec.state in ("draft", "sent"):
+            return "pending"
+        else:
+            return "processing"
+
+    @classmethod
     def from_sale_order(cls, odoo_rec):
         res = super().from_sale_order(odoo_rec)
-        res.state = odoo_rec.shopinvader_state
+        res.state = cls._get_futur_state(odoo_rec)
         return res
