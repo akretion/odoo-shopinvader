797007b0f87282ac42a55643699b19e03b700453
Florian Mounier <paradoxxx.zero@gmail.com>
2025-03-11 13:50:58 +0100

    [ADD] shopinvader_auth_jwt_transfer_cart_coupon

---
+++ shopinvader_auth_jwt_transfer_cart_coupon/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Shopinvader JWT Authentication Tranfer Cart Coupon",
+    "version": "14.0.1.0.0",
+    "summary": "Sale Coupon Cart Transfer Support",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": [
+        "shopinvader_auth_jwt_transfer_cart",
+        "shopinvader_sale_coupon",
+    ],
+    "auto_install": True,
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+}
---
+++ shopinvader_auth_jwt_transfer_cart_coupon/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds sale_coupon support for the cart service transfer.
