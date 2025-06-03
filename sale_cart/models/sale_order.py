db27aeda4127047ceee582c380b656751a582e76
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-06-03 15:10:08 +0200

    [IMP] sale_cart: add ondelete strategy on typology field

--- sale_cart/models/sale_order.py
+++ sale_cart/models/sale_order.py
@@ -5,10 +5,14 @@ from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-
     _inherit = "sale.order"
 
-    typology = fields.Selection(selection_add=[("cart", "Cart")])
+    typology = fields.Selection(
+        selection_add=[("cart", "Cart")],
+        ondelete={
+            "cart": "cascade",
+        },
+    )
 
     def _confirm_cart(self):
         self.ensure_one()
