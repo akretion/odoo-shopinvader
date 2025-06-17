cf084ba6dd88984f29c298d25cfc602beae432cd
Francois Poizat <francois.poizat@gmail.com>
2025-06-17 16:09:30 +0200

    [ADD] shopinvader_restapi_helpdeks

---
+++ shopinvader_restapi_helpdesk/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Invader Helpdesk",
+    "summary": "Integrate Helpdesk into Shopinvader",
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "shopinvader",
+        "helpdesk_mgmt_rest_api",  # OCA version
+        "helpdesk_mgmt_sale",  # Sale info on helpdesk tickets
+    ],
+    "external_dependencies": {
+        "python": [
+            "pydantic",
+            "pydantic[email]",
+            "extandle_pydantic",
+        ]
+    },
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+    "qweb": [],
+}
---
+++ shopinvader_restapi_helpdesk/models/__init__.py
@@ -0,0 +1,3 @@
+from . import settings
+from . import shopinvader_backend
+from . import ticket
