1670add610b11d1d595c73421b1760b9f800f30c
Florian Mounier <paradoxxx.zero@gmail.com>
2025-09-11 13:24:43 +0200

    [IMP] shopinvader-sale-channel: Add get_current sale channel method

--- shopinvader_sale_channel/models/sale_channel.py
+++ shopinvader_sale_channel/models/sale_channel.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleChannel(models.Model):
@@ -12,3 +12,20 @@ class SaleChannel(models.Model):
     channel_type = fields.Selection(
         selection_add=[("shopinvader", "Shopinvader")],
     )
+
+    @api.model
+    def get_current(self):
+        # TODO: PR in sale-channel
+        # sale_channel = super().get_current()
+        sale_channel = False
+        if not sale_channel:
+            sale_channel = self._get_current_fastapi_sale_channel()
+        return sale_channel
+
+    def _get_current_fastapi_sale_channel(self):
+        fastapi_endpoint_id = self.env.context.get("fastapi_endpoint_id")
+        if fastapi_endpoint_id:
+            return (
+                self.env["fastapi.endpoint"].browse(fastapi_endpoint_id).sale_channel_id
+            )
+        return None
